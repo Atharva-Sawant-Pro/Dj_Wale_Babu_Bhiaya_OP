85bac214ba1afe32f87323e2579b066df7dc2485
Atharva-Sawant-Pro <78305129+Atharva-Sawant-Pro@users.noreply.github.com>
2021-05-07 17:01:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,7 +9,24 @@ function setup() {
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
 
+}
+function modelLoaded(){
+    console.log("Moodeel Loodaad");
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        rightWristX=results[0].pose.rightWrist.x;
+        rightWristY=results[0].pose.rightWrist.y;
+
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+        console.log("rightWristX="+rightWristX+"rightWristY="+rightWristY);
+        console.log("leftWristX="+leftWristX+"leftWristY="+leftWristY);
+    }
 }
 function draw() {
     image(video, 0, 0, 600, 500);
